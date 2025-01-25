871b224258350ee940a2b9660312af6dbe3be1f1
InfinityJuice <ryan.wang.2022.me@gmail.com>
2025-01-25 14:59:23 -0500

    Advantagekit vision template working now.
    Co-authored-by: Shreshta Parthaje <shreshtaparthaje@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Vision/VisionInput.java
@@ -0,0 +1,20 @@
+// package frc.robot.subsystems.Vision;
+
+// import org.littletonrobotics.junction.AutoLog;
+// import org.littletonrobotics.junction.LogTable;
+// import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+// import edu.wpi.first.math.geometry.Rotation2d;
+// import frc.robot.subsystems.Vision.VisionIO.PoseObservation;
+// import frc.robot.subsystems.Vision.VisionIO.TargetObservation;
+
+// public class VisionInput {
+//         @AutoLog
+//     public static class VisionIOInputs {
+//         public boolean connected = false;
+//         public TargetObservation latestTargetObservation =
+//             new TargetObservation(new Rotation2d(), new Rotation2d());
+//         public PoseObservation[] poseObservations = new PoseObservation[0];
+//         public int[] tagIds = new int[0];
+//   }
+// }
