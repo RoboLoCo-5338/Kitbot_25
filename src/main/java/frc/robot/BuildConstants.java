fefb61e413df114f70c9837913597dcfc96423df
Kavin Muralikrishnan <921563@lcps.org>
2025-04-11 12:57:20 -0400

    preliminary advantagekit stuff

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Kitbot-25";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "3968b93312ec37f53911cba0675ec84b362a04dc";
+  public static final String GIT_DATE = "2025-04-11 09:29:24 EDT";
+  public static final String GIT_BRANCH = "Akit";
+  public static final String BUILD_DATE = "2025-04-11 12:56:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1744390619690L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,33 +1,22 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public class Constants {
-	public static final double scorePreset = 85;
-	public static final double sourcePreset = 0;
-	public static final double groundPreset = 115;
-	public static final double stackPreset = 110;
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+  public static final double ROBOT_LENGTH = Units.inchesToMeters(33.250000);
+  public static final double FLOOR_TO_MECHANISM = Units.inchesToMeters(8);
 
-	public static final class RollerConstants {
-		public static final int ROLLER_MOTOR_ID = 25;
-		// public static final double rollerMotorSpeed = 0.35;
-		public static double rollerMotorSpeed = Preferences.getDouble("rollerMotorSpeed", 0.0);
-		// public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
-		// public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-		// public static final double ROLLER_EJECT_VALUE = 0.44;
-		public static void reloadConstants() {
-			rollerMotorSpeed = Preferences.getDouble("rollerMotorSpeed", 0.0);
-		}
-	}
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
 
-	public static void initPreferences() {
-		String[] subsystemIdentifiers = {"rollerMotor"};
-		for (String id : subsystemIdentifiers) {
-			Preferences.initDouble(id + "Speed", 0);
-		}
-	}
+    /** Running a physics simulator. */
+    SIM,
 
-	public static void reloadPreferences() {
-		RollerConstants.reloadConstants();
-	}
+    /** Replaying from a log file. */
+    REPLAY
+  }
 }
