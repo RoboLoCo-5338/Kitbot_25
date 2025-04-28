d782e17675286bf39fd969c8cf106de7abc10cc1
Kavin Muralikrishnan <mario.williams927@gmail.com>
2025-04-28 21:44:18 +0000

    Automated Spotless Apply

--- src/main/java/frc/robot/subsystems/gamepiecedetection/GamePieceDetectionIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/gamepiecedetection/GamePieceDetectionIOPhotonVision.java
@@ -38,16 +38,26 @@ public class GamePieceDetectionIOPhotonVision implements GamePieceDetectionIO {
     for (PhotonPipelineResult result : camera.getAllUnreadResults()) {
       for (PhotonTrackedTarget target : result.getTargets()) {
         // TODO: Figure out what targetHeight and targetPitch should be for game pieces
-        //Calculates the translation from the camera to the target
-        Translation2d cameraToTarget = PhotonUtils.estimateCameraToTargetTranslation(
-            PhotonUtils.calculateDistanceToTargetMeters(
-                Constants.FLOOR_TO_MECHANISM + robotToCamera.getZ(),
-                0,
-                robotToCamera.getRotation().getY(),
-                0),
-            Rotation2d.fromRadians(-target.getYaw()));
-          //Transforms the robot's pose by the camera's pose and then transforms it by the camera to target translation, therefore getting the pose of the target
-          Pose2d targetPose = robotPose.get().plus(new Transform2d(robotToCamera.getX(), robotToCamera.getY(), new Rotation2d(robotToCamera.getRotation().getX()))).plus(new Transform2d(cameraToTarget, robotPose.get().times(-1).getRotation()));
+        // Calculates the translation from the camera to the target
+        Translation2d cameraToTarget =
+            PhotonUtils.estimateCameraToTargetTranslation(
+                PhotonUtils.calculateDistanceToTargetMeters(
+                    Constants.FLOOR_TO_MECHANISM + robotToCamera.getZ(),
+                    0,
+                    robotToCamera.getRotation().getY(),
+                    0),
+                Rotation2d.fromRadians(-target.getYaw()));
+        // Transforms the robot's pose by the camera's pose and then transforms it by the camera to
+        // target translation, therefore getting the pose of the target
+        Pose2d targetPose =
+            robotPose
+                .get()
+                .plus(
+                    new Transform2d(
+                        robotToCamera.getX(),
+                        robotToCamera.getY(),
+                        new Rotation2d(robotToCamera.getRotation().getX())))
+                .plus(new Transform2d(cameraToTarget, robotPose.get().times(-1).getRotation()));
       }
     }
   }
